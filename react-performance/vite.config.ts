f4262a8278b798b2669503c4576d98a51436b21c
Maya Zakharova <mayzakharova@gmail.com>
2025-03-23 10:32:29 -0500

    refactor: fix confiruration files for deploy

--- react-performance/vite.config.ts
+++ react-performance/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/React-Performance/',
   define: {
     "process.env.NODE_ENV": JSON.stringify("development"),
   },
